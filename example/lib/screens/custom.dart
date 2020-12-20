f9fc97a66e9e9a5b16f423fde935449035993b1f
theiskaa <59066341+theiskaa@users.noreply.github.com>
2020-12-20 15:10:14 +0400

    like button added

--- example/lib/screens/custom.dart
+++ example/lib/screens/custom.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_buttons/customButtons/hover_button.dart';
+import 'package:flutter_buttons/customButtons/like_button.dart';
 
 class Custom extends StatelessWidget {
   const Custom({Key key}) : super(key: key);
@@ -7,41 +8,79 @@ class Custom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      padding: EdgeInsets.symmetric(vertical:100),
+      padding: EdgeInsets.symmetric(vertical: 50),
       child: Column(
         children: [
-          // Default Mode
-          HoverButton(
-            title: "Hover Button",
-            onTap: () {},
-          ),
+          buildHoverButtons(),
           SizedBox(height: 50),
-          // Hover button with custom spalsh color,custom title size and border radius
-          HoverButton(
-            onTap: () {},
-            title: "Buy Now",
-            spashColor: Colors.red,
-            titleSize: 20,
-            borderRadius: BorderRadius.circular(30),
-          ),
+          buildDivider(),
           SizedBox(height: 50),
-          // And this is all options used version
-          HoverButton(
-            onTap: () {},
-            title: "Add To Cart",
-            titleSize: 30,
-            titleColor: Colors.orange[900],
-            spashColor: Colors.yellow,
-            tappedTitleColor: Colors.black,
-            fontWeight: FontWeight.bold,
-            borderColor: Colors.orange[900],
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
-            ),
+          Column(
+            children: [
+              //Default mode
+              LikeButton(
+                onTap: () {},
+              ),
+
+              /// Attention! if you do some changing on like button you must to do [hot reolad] for view changes
+              // Like Button with fully options
+              LikeButton(
+                icon: Icons.home,
+                deactiveColor: Colors.grey,
+                activeColor: Colors.purple,
+                deactiveSize: 50,
+                activeSize: 55,
+                curve: Curves.easeInExpo,
+                onTap: () {},
+              ),
+            ],
           ),
         ],
       ),
+    );
+  }
+
+  Column buildHoverButtons() {
+    return Column(
+      children: [
+        // Default Mode
+        HoverButton(
+          title: "Hover Button",
+          onTap: () {},
+        ),
+        SizedBox(height: 50),
+        // Hover button with custom spalsh color,custom title size and border radius
+        HoverButton(
+          onTap: () {},
+          title: "Buy Now",
+          spashColor: Colors.red,
+          titleSize: 20,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        SizedBox(height: 50),
+        // And this is all options used version
+        HoverButton(
+          onTap: () {},
+          title: "Add To Cart",
+          titleSize: 30,
+          titleColor: Colors.orange[900],
+          spashColor: Colors.yellow,
+          tappedTitleColor: Colors.black,
+          fontWeight: FontWeight.bold,
+          borderColor: Colors.orange[900],
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Container buildDivider() {
+    return Container(
+      height: 2,
+      color: Colors.black,
     );
   }
 }
